95f454882b8b8f65ffc900fe587431439da7865e
anshu20126 <anshu20126@gmail.com.com>
2020-07-13 18:35:54 +0530

    form based authin

--- demo/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ demo/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.server.ui.LoginPageGeneratingWebFilter;
 
 import static com.example.demo.security.ApplicationUserRole.*;
 
@@ -34,9 +35,9 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception{
 		http 
-		 .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-          .and() 
-         // .csrf().disable()
+		 //.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+          //.and() 
+          .csrf().disable()
 		    .authorizeRequests()
 		    .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
 		    .antMatchers( "/api/*").hasRole(STUDENT.name())
@@ -47,7 +48,13 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter{
 		    .anyRequest()
 		    .authenticated()
 		    .and()
-		    .httpBasic();
+		    .formLogin()
+		  .loginPage("/login").permitAll()
+		  .defaultSuccessUrl("/courses",true)
+		  .and()
+		  .rememberMe();//defaults to 2 weeks
+		
+		    
 		
 	}
 	@Override
