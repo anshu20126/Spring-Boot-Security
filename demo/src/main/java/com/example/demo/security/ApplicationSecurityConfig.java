98088f6324021a61c770257f4790499ce752123f
anil20126 <anil.kumar@robosoftin.com>
2020-07-14 18:31:00 +0530

    database authentication

--- demo/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ demo/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -1,25 +1,22 @@
 package com.example.demo.security;
 
+import com.example.demo.auth.ApplicationUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.task.TaskTimeoutException;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.security.web.server.ui.LoginPageGeneratingWebFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.util.concurrent.TimeUnit;
 
 import static com.example.demo.security.ApplicationUserRole.*;
 
-import java.util.concurrent.TimeUnit;
 
 
 @Configuration
@@ -29,11 +26,15 @@ import java.util.concurrent.TimeUnit;
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	private final PasswordEncoder passwordEncoder;
+    private final ApplicationUserService applicationUserService;
+
 
     @Autowired
-    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
-        this.passwordEncoder = passwordEncoder;
-    }
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder,
+            ApplicationUserService applicationUserService) {
+            this.passwordEncoder = passwordEncoder;
+            this.applicationUserService = applicationUserService;
+}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception{
@@ -71,9 +72,22 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter{
             .deleteCookies("JSESSIONID", "remember-me")
             .logoutSuccessUrl("/login");
 }
-		
+	
+	 @Override
+	    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	        auth.authenticationProvider(daoAuthenticationProvider());
+	    }
+
+	    @Bean
+	    public DaoAuthenticationProvider daoAuthenticationProvider() {
+	        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+	        provider.setPasswordEncoder(passwordEncoder);
+	        provider.setUserDetailsService(applicationUserService);
+	        return provider;
+	    }
 		    
-	@Override
+	
+	/*@Override
     @Bean
     protected UserDetailsService userDetailsService() {
 		UserDetails annaSmithUser = User.builder()
@@ -104,5 +118,5 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter{
 		        		tomUser);
 		        
 
-	}
+	}*/
 }
